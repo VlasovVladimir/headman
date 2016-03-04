f045668bd6445c7ffab66b5ac064d708fc0bc355
VlasovVladimir <vpvlasovhse@gmail.com>
2016-03-04 12:26:48 +0300

    Added empty forms

---
+++ headman/Forms/Maps/PatternMap.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace headman.Forms.Maps
+{
+    /// <summary>
+    /// Interaction logic for PatternMap.xaml
+    /// </summary>
+    public partial class PatternMap : Window
+    {
+        public PatternMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
