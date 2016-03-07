2dba2d07f7e2055ce8c89536a73a55b09c8d4a70
VlasovVladimir <vpvlasovhse@gmail.com>
2016-03-07 19:36:56 +0300

    MiniMenu

--- headman/Places.cs
+++ headman/Places.cs
@@ -1,3 +1,4 @@
 ﻿// http://365psd.ru/catalog/1003 -- дерево
 // https://www.wanderable.com/hm/weredoinit -- вулкан 1
 // http://ru.aliexpress.com/item/landscape-volcano-fire-flames-lava-jungle-forest-ocean-waves-night-moon-moonlight-airbrushing-4-Size-Decor/32470503528.html?spm=2114.41010708.4.12.npIXkP -- вулкан 2
+// http://freeguides.me/?p=5394 - village
