2a37bc4dfc9f27d74b48e87a7a7ef7fc8a54e0bf
VlasovVladimir <vpvlasovhse@gmail.com>
2016-03-08 00:17:31 +0300

    TimeStarted

--- headman/Forms/Maps/PatternMap.xaml.cs
+++ headman/Forms/Maps/PatternMap.xaml.cs
@@ -22,6 +22,42 @@ namespace headman.Forms.Maps
         public PatternMap()
         {
             InitializeComponent();
+            Timer = 0;
+            Speed = 1;
+            MonthNum = 1;
+            Timing.Text = "Месяц №" + MonthNum.ToString();
+            MonthFinished += (object obj) => {this.Timing.Text = "Месяц №" + MonthNum.ToString(); MonthNum+=1;};
         }
+
+        private int Timer { get; set; }
+        private int Speed { get; set; }
+        private int MonthNum { get; set; }   
+
+        Action <object> MonthFinished;
+
+        private async void TimeRun ()
+        {
+            while (true)
+            {
+                if (Timer<10)
+                {
+                    Timer += Speed;
+                }
+                else
+                {
+                    Timer = 0;
+                    MonthFinished(null);
+                }
+                await Task.Delay(100);
+            }
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            TimeRun();
+        }
+
+
     }
 }
