895c43c542e7f25d817737efaa59c2276a2fa055
MaryGerda <myupolyakova_1@edu.hse.ru>
2016-03-07 19:54:56 +0300

    RegionInfoPlusPlain

---
+++ headman/Region/RegionInfo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace headman.Region
+{
+    /// <summary>
+    /// Логика взаимодействия для RegionInfo.xaml
+    /// </summary>
+    public partial class RegionInfo : Window
+    {
+        public RegionInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
