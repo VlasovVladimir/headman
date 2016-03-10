615a29bdb01a6bf239f2f09516b8313045c0c563
VlasovVladimir <vpvlasovhse@gmail.com>
2016-03-10 13:43:56 +0300

    IEventFabric&CurrentMoment

--- headman/СurrentMoment/CurrentMoment.cs
+++ headman/СurrentMoment/CurrentMoment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using headman.Region;
 
 namespace headman.СurrentMoment
 {
@@ -14,6 +14,8 @@ namespace headman.СurrentMoment
         public int Wood { get; set; }
         public int Stone { get; set; }
 
-        pub
+        public List<IRegion> Regions {get; set;}
+
+
     }
 }
