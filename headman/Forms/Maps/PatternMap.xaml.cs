95ad1578fd78afd7dd1945b6072e0a673d01f2e5
VlasovVladimir <vpvlasovhse@gmail.com>
2016-03-16 02:18:29 +0300

    Colours&Buttons

--- headman/Forms/Maps/PatternMap.xaml.cs
+++ headman/Forms/Maps/PatternMap.xaml.cs
@@ -41,21 +41,24 @@ namespace headman.Forms.Maps
             Randomizator = new Random();
             currentEvent = null;
             Pause.IsEnabled = false;
-            TikTacSpeed = 500;
+            TikTacSpeed = 2000;
 
             MiniMenuOpened = false;
 
             this.InfoRefresh();
-            Timing.Text = "Месяц №" + RepositorySingle.currentSituation.GameMonth.ToString();
-            MonthFinished += () => {RepositorySingle.currentSituation.GameMonth+=1; this.Timing.Text = "Месяц №" + RepositorySingle.currentSituation.GameMonth.ToString(); };
+            Timing.Text = "Месяц  №" + RepositorySingle.currentSituation.GameMonth.ToString();
+            MonthFinished += () => { RepositorySingle.currentSituation.GameMonth += 1; this.Timing.Text = "Месяц №" + RepositorySingle.currentSituation.GameMonth.ToString(); };
             MonthFinished += EventCaller;
+            MonthFinished += StandartTurn;
         }
+
+
 
         private int Timer { get; set; }
         private int Speed { get; set; }
         private int TikTacSpeed { get; set; }
         private int RandomNum { get; set; }
-        
+
         private bool TimeStarted { get; set; }
         public bool MiniMenuOpened { get; set; }
 
@@ -64,11 +67,11 @@ namespace headman.Forms.Maps
 
         private IEvent currentEvent;
 
-        private async void TimeRun ()  // главный цикл программы
+        private async void TimeRun()  // главный цикл программы
         {
             while (true)
             {
-                if (Timer<10)
+                if (Timer < 10)
                 {
                     Timer += Speed;
                 }
@@ -90,7 +93,7 @@ namespace headman.Forms.Maps
                 Speed = 1;
             }
             else
-            { 
+            {
                 TimeRun();
                 Speed = 1;
             }
@@ -136,7 +139,7 @@ namespace headman.Forms.Maps
             MiniMenuOpened = false;
         }
 
-        private void EventCaller ()
+        private void EventCaller()
         {
             int decision = (Randomizator).Next(100);
             if ((decision >= 91) && (RepositorySingle.currentSituation.BadEvents.Count != 0))
@@ -152,14 +155,14 @@ namespace headman.Forms.Maps
                     currentEvent = RepositorySingle.currentSituation.GoodEvents[RandomNum];
                     RepositorySingle.currentSituation.GoodEvents.RemoveAt(RandomNum);
                 }
-            
-            if (currentEvent!=null)
+
+            if (currentEvent != null)
             {
                 currentEvent.Moment = RepositorySingle.currentSituation;
                 EventMenuForm curentEventMenu = new EventMenuForm(currentEvent);
                 Pause_Click(null, null);
                 curentEventMenu.ShowDialog();
-                
+
                 Log.Text += "Месяц №" + RepositorySingle.currentSituation.GameMonth.ToString() + ". " + currentEvent.Log + "\n";
                 currentEvent = null;
                 Start_Click(null, null);
@@ -169,7 +172,7 @@ namespace headman.Forms.Maps
         // обработка нажатия на крестик сверху
         private void MapClose(object sender, EventArgs e)
         {
-            if(!MiniMenuOpened)
+            if (!MiniMenuOpened)
                 Application.Current.Shutdown();
         }
 
@@ -191,6 +194,193 @@ namespace headman.Forms.Maps
             WoodenInfo.Text = RepositorySingle.currentSituation.Wood.ToString();
             WaterInfo.Text = RepositorySingle.currentSituation.Water.ToString();
             PeopleInfo.Text = RepositorySingle.currentSituation.Population.ToString();
+
+            bool[] items = RepositorySingle.currentSituation.Items;
+
+            #region ButtonsAndColours
+
+            if (items[0])
+            {
+                Smile0.Background = new SolidColorBrush(Colors.Green);
+                Smile0.BorderBrush = new SolidColorBrush(Colors.Green);
+                Smile0.Text = " ☺";
+                Get0.IsEnabled = false;
+
+            }
+            else
+            {
+                Smile0.Background = new SolidColorBrush(Colors.Red);
+                Smile0.BorderBrush = new SolidColorBrush(Colors.Red);
+                Smile0.Text = " ☹";
+                Get0.IsEnabled = true;
+            }
+
+            if (items[1])
+            {
+                Smile1.Background = new SolidColorBrush(Colors.Green);
+                Smile1.BorderBrush = new SolidColorBrush(Colors.Green);
+                Smile1.Text = " ☺";
+                Get1.IsEnabled = false;
+
+            }
+            else
+            {
+                Smile1.Background = new SolidColorBrush(Colors.Red);
+                Smile1.BorderBrush = new SolidColorBrush(Colors.Red);
+                Smile1.Text = " ☹";
+                Get1.IsEnabled = true;
+            }
+
+            if (items[2])
+            {
+                Smile2.Background = new SolidColorBrush(Colors.Green);
+                Smile2.BorderBrush = new SolidColorBrush(Colors.Green);
+                Smile2.Text = " ☺";
+                Get2.IsEnabled = false;
+
+            }
+            else
+            {
+                Smile2.Background = new SolidColorBrush(Colors.Red);
+                Smile2.BorderBrush = new SolidColorBrush(Colors.Red);
+                Smile2.Text = " ☹";
+                Get2.IsEnabled = true;
+            }
+
+            if (items[3])
+            {
+                Smile3.Background = new SolidColorBrush(Colors.Green);
+                Smile3.BorderBrush = new SolidColorBrush(Colors.Green);
+                Smile3.Text = " ☺";
+                Get3.IsEnabled = false;
+
+            }
+            else
+            {
+                Smile3.Background = new SolidColorBrush(Colors.Red);
+                Smile3.BorderBrush = new SolidColorBrush(Colors.Red);
+                Smile3.Text = " ☹";
+                Get3.IsEnabled = true;
+            }
+
+            if (items[4])
+            {
+                Smile4.Background = new SolidColorBrush(Colors.Green);
+                Smile4.BorderBrush = new SolidColorBrush(Colors.Green);
+                Smile4.Text = " ☺";
+                Get4.IsEnabled = false;
+
+            }
+            else
+            {
+                Smile4.Background = new SolidColorBrush(Colors.Red);
+                Smile4.BorderBrush = new SolidColorBrush(Colors.Red);
+                Smile4.Text = " ☹";
+                Get4.IsEnabled = true;
+            }
+
+            if (items[5])
+            {
+                Smile5.Background = new SolidColorBrush(Colors.Green);
+                Smile5.BorderBrush = new SolidColorBrush(Colors.Green);
+                Smile5.Text = " ☺";
+                Get5.IsEnabled = false;
+
+            }
+            else
+            {
+                Smile5.Background = new SolidColorBrush(Colors.Red);
+                Smile5.BorderBrush = new SolidColorBrush(Colors.Red);
+                Smile5.Text = " ☹";
+                Get5.IsEnabled = true;
+            }
+            #endregion
+
+        }
+
+        private void StandartTurn()
+        {
+
+            int people = this.RepositorySingle.currentSituation.Population;
+            if (people <= 0)
+            {
+                Pause_Click(null, null);
+                Start.IsEnabled = false;
+                SpeedUp.IsEnabled = false;
+                MessageBox.Show("GAME OVER");
+            }
+
+            if (people <= 50)
+                this.RepositorySingle.currentSituation.Water -= 1;
+            else if (people <= 100)
+                this.RepositorySingle.currentSituation.Water -= 2;
+            else
+                this.RepositorySingle.currentSituation.Water -= 3;
+
+            if (this.RepositorySingle.currentSituation.Water < 0)
+            {
+                people -= 1;
+                this.RepositorySingle.currentSituation.Water = 0;
+            }
+
+            if (!RepositorySingle.currentSituation.Items[4])
+                people -= 1;
+
+
+            this.RepositorySingle.currentSituation.Population = people;
+
+            GetStones.IsEnabled = true;
+            GetWood.IsEnabled = true;
+            GetWater.IsEnabled = true;
+
+        }
+
+        private void MoveToAnotherIsland()
+        {
+
+        }
+
+        #region Get_Resourses
+        private void GetStones_Click(object sender, RoutedEventArgs e)
+        {
+            if (RepositorySingle.currentSituation.Items[1])
+                RepositorySingle.currentSituation.Stone += 10;
+            else
+                RepositorySingle.currentSituation.Stone += 5;
+
+            GetStones.IsEnabled = false;
+            GetWood.IsEnabled = false;
+            GetWater.IsEnabled = false;
+        }
+
+        private void GetWood_Click(object sender, RoutedEventArgs e)
+        {
+            if (RepositorySingle.currentSituation.Items[1])
+                RepositorySingle.currentSituation.Wood += 10;
+            else
+                RepositorySingle.currentSituation.Wood += 5;
+
+            GetStones.IsEnabled = false;
+            GetWood.IsEnabled = false;
+            GetWater.IsEnabled = false;
+        }
+
+        private void GetWater_Click(object sender, RoutedEventArgs e)
+        {
+            if (RepositorySingle.currentSituation.Items[2])
+                RepositorySingle.currentSituation.Water = 20;
+            else
+                RepositorySingle.currentSituation.Water = 10;
+
+            GetStones.IsEnabled = false;
+            GetWood.IsEnabled = false;
+            GetWater.IsEnabled = false;
+        }
+        #endregion
+
+        private void Get0_Click(object sender, RoutedEventArgs e)
+        {
+            RepositorySingle.currentSituation.Items[0] = true;
         }
     }
 }
