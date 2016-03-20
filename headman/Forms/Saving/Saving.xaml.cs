6e16f9459d9760e085ef4d6672fca053d1db2ef4
VlasovVladimir <vpvlasovhse@gmail.com>
2016-03-20 13:02:27 +0300

    SavesFix

--- headman/Forms/Saving/Saving.xaml.cs
+++ headman/Forms/Saving/Saving.xaml.cs
@@ -125,30 +125,56 @@ namespace headman.Forms.Saving
                 {
                     operatingSave = (CurrentMoment)(ser.Deserialize(fs));
                 }
-        }
-
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            XmlSerializer ser = new XmlSerializer(typeof(CurrentMoment));
-            CurrentMoment savingMoment= RepositirySingle.currentSituation;
-            savingMoment.SaveDate = DateTime.Now;
-
-            if (this.Name.Text !=null)
-                using (FileStream fs = new FileStream(DirectoryPath + this.Name.Text + ".xml", FileMode.OpenOrCreate))
-                {
-                    ser.Serialize(fs, savingMoment);
-                    MessageBox.Show("Готово");
-                    using (StreamWriter sw = new StreamWriter (new FileStream(DirectoryPath + "list.txt", FileMode.Append, FileAccess.Write)))
-                    {
-                        sw.WriteLine(Name.Text);
-                    }
-                }
             else
             {
                 operatingSave = null;
                 MessageBox.Show("Шаловливые духи украли карту! Ваше племя потерялось в океане.", "Ошибка...", MessageBoxButton.OK);
             }
 
+
+        }
+
+        private void Save (string name)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(CurrentMoment));
+            CurrentMoment savingMoment = RepositirySingle.currentSituation;
+            savingMoment.SaveDate = DateTime.Now;
+
+            if (name != null)
+                using (FileStream fs = new FileStream(DirectoryPath + name + ".xml", FileMode.OpenOrCreate))
+                {
+                    ser.Serialize(fs, savingMoment);
+                    MessageBox.Show("Готово");
+                    using (StreamWriter sw = new StreamWriter(new FileStream(DirectoryPath + "list.txt", FileMode.Append, FileAccess.Write)))
+                    {
+                        sw.WriteLine(name);
+                    }
+                }
+            else
+            {
+                MessageBox.Show("Надо бы ввести название карты!", "Ошибка...", MessageBoxButton.OK);
+            }
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (RepositirySingle.currentSituation != null)
+            {
+                if (SavingList.Items.Contains(Name.Text))
+                {
+
+                    MessageBoxResult result = MessageBox.Show("Уже есть такое сохранение. Перезаписать?", "У нас вопрос!", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        this.Save(Name.Text);
+                    }
+                }
+                else
+                {
+                    this.Save(Name.Text);
+                }
+            }
         }
 
         private void SavingList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -156,8 +182,11 @@ namespace headman.Forms.Saving
             this.LoadMoment(SavingList.SelectedItem.ToString());
             if (operatingSave != null)
             {
-                Info.Text = SavingList.SelectedItem.ToString() + "  " + operatingSave.SaveDate.ToString() + "\n" +
-                    "Население и так далее...";
+                Info.Text = SavingList.SelectedItem.ToString() + "  " + operatingSave.SaveDate.ToString() + "\n" + "\n" +
+                    "Название карты: " + operatingSave.MapName + "\n" +
+                    "Численность племени: " + operatingSave.Population.ToString()+ "\n" +
+                    "Текущая локация: " + operatingSave.Regions[operatingSave.CurrentRegionIndex].Name + "\n" +
+                    "Шансы на победу: в этой игре вообще вряд ли возможно выиграть";
             }
             Name.Text = SavingList.SelectedItem.ToString();
         }
