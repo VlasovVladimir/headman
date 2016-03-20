9557c8b01bf9da5a25c38ac10cc9585eb0ef56f4
MaryGerda <myupolyakova_1@edu.hse.ru>
2016-03-20 18:59:04 +0300

    Refs

--- headman/Places.cs
+++ headman/Places.cs
@@ -3,8 +3,6 @@
 // http://ru.aliexpress.com/item/landscape-volcano-fire-flames-lava-jungle-forest-ocean-waves-night-moon-moonlight-airbrushing-4-Size-Decor/32470503528.html?spm=2114.41010708.4.12.npIXkP -- вулкан 2
 // http://freeguides.me/?p=5394 - village
 // http://getbg.net/texture/41.html - water
-
-// стартовое окно
-// победное окно
-// справка
-// 
+// http://ru.depositphotos.com/9768270/stock-illustration-vector-fireworks-over-a-city.html - салют 1
+// http://svidetel24.info/archives/20462 - лодка
+// https://i.ytimg.com/vi/NCgr2D9w6tM/maxresdefault.jpg - салют 2
