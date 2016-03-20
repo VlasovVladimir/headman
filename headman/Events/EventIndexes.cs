c3c2f9700b3e9398c820e59794efe4e14b483a98
VlasovVladimir <vpvlasovhse@gmail.com>
2016-03-20 18:24:43 +0300

    Rename&Effects (Masha)

--- headman/Events/EventIndexes.cs
+++ headman/Events/EventIndexes.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace headman.Event
 {
-    public class EventIndexs
+    public class EventIndexes
     {
-        public List<int> GoodEventsIndexs()
+        public List<int> GoodEventsIndexes()
         {
             return new List<int> { 91, 92, 93, 94, 95, 96, 13, 14, 15, 16, 17, 70, 51, 52, 53, 20 };
         }
         
-        public List<int> BadEventsIndexs()
+        public List<int> BadEventsIndexes()
         {
             return new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 22, 54, 11, 12, 18, 19 };
         }
